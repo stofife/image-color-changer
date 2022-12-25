bb128673e8a316ee96cf288bd1216ae45c27f0b6
stofife <stefandubnicka315@gmail.com>
2022-12-25 14:03:21 +0100

    Is now faster. (No longer using square root)

--- colors.py
+++ colors.py
@@ -6,7 +6,7 @@ img = Image.open("exp.png", "r")
 pixels = list(img.getdata())
 pixels = [pixel[:3] for pixel in pixels]
 
-ld = 46
+ld = 46**2
 
 c_counter = Counter(pixels)
 c_sorted = c_counter.most_common()
@@ -22,7 +22,7 @@ while len(colors) < 16:
     dist = []
     for color in colors:
         p1, p2 = np.array(c), np.array(color)
-        dist.append(np.sqrt(np.sum((p1 - p2)**2, axis=0)))
+        dist.append(np.sum((p1 - p2)**2, axis=0))
     cur_top += 1
     if not (min(dist) < ld):
         colors.append(c)
@@ -37,7 +37,7 @@ for i in range(len(pixels)):
     p1 = np.array(pixels[i])
     for color in colors:
         p2 = np.array(color)
-        dist = np.sqrt(np.sum((p1 - p2)**2, axis=0))
+        dist = np.sum((p1 - p2)**2, axis=0)
         if (not min_dist) or dist < min_dist[0]:
             min_dist = (dist, color)
     new_pixels[i] = min_dist[1]
